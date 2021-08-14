72f228bf531edc51a7cfa73919618c478757c04e
Ritesham1 <ritesham@gmail.com>
2021-08-14 17:07:08 +0530

    I LOVE VICTORY

--- hello.sh
+++ hello.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
 
-n=0
+# n=0
 
-while [ $n -le 10 ];do
-    echo "hello cloudoric"
-    n="$((n+1))"
+# while [ $n -le 10 ];do
+#     echo "hello cloudoric"
+#     n="$((n+1))"
+# done
+for i in {1,2,3,4,5}
+do
+    echo " CLOUDORICH"
 done
